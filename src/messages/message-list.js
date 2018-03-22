c6fb26ecf944e4921e5e0ef5f23034c1376dd318
Matt Holloway <hollowmatt@gmail.com>
2018-03-21 20:39:18 -0400

    Added multi-message, more css

--- src/messages/message-list.js
+++ src/messages/message-list.js
@@ -4,18 +4,37 @@ import MessageView from './message-view';
 class MessageList extends Component {
 
 	state = {
-		message: {
-			from: 'Martha',
-			content: 'I will be traveling soon',
-			status: 'read'
-		}
+		messages: [
+			{
+				from: 'Martha',
+				content: 'I will be traveling soon',
+				status: 'read'
+			},
+			{
+				from: 'John',
+				content: 'Event starts soon',
+				status: 'unread'
+			},
+			{
+				from: 'Jacob',
+				content: 'Talk later, have fun.',
+				status: 'read'
+			}
+		]
 	}
 
 	render() {
-		return (
+		const messageViews = this.state.messages.map(function(message, index){
+			return (
+			<div className="note">
+				<MessageView key={index} message={message} />
+			</div>
+		)	
+		})
+		return(
 			<div>
 				<h1>List of Messages</h1>
-				<MessageView message={this.state.message} />
+				{messageViews}
 			</div>
 		)
 	}
